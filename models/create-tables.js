cc360b932524d2cad8fe963ed6536d208f8e8222
Yan Christoffer <yandepaula@gmail.com>
2017-07-01 11:49:46 -0300

    update fix pass

--- models/create-tables.js
+++ models/create-tables.js
@@ -6,5 +6,5 @@ const query = client.query(
   'CREATE TABLE if not exists artigos(Id SERIAL PRIMARY KEY, Titulo VARCHAR(50) not null,Dono varchar(25) not null,artigo text not null);INSERT INTO artigos(Titulo,Dono,artigo) VALUES(\'Bem Vindo\',\'yccp\',\'Obrigado por nos prestigiar com seu artigo\');');
 query.on('end', () => { client.end(); });
 const query2 = client.query(
-  'CREATE TABLE if not exists users(login varchar(20) PRIMARY KEY, senha VARCHAR(16) not null,nome varchar(25));;INSERT INTO users(login,nome,senha) VALUES(\'yccp\',\'Yan Christoffer\',\'yanc0302\');');
+  'CREATE TABLE if not exists users(login varchar(20) PRIMARY KEY, senha text not null,nome varchar(25));');
 query.on('end', () => { client.end(); });
