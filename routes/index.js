e4cc58622594d0e37bb0b2792b3c89bab702e83d
Yan Christoffer <yandepaula@gmail.com>
2017-07-06 14:44:21 -0300

    FIXING A PROBLEM

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,9 @@ router.get('/search/:pc',function(req,res,next){
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }
+     const search={pc:"%"+req.params.Dono+"%"};
    // SQL Query > Select Data
-   const query=client.query("select * from artigos where artigo like $1 or titulo like $1  order by id desc",["%"+req.params.pc+"%"],(err, resp) => {
+    const query=client.query("select * from artigos where artigo LIKE $1 OR Titulo LIKE $1 order by id desc",[search.pc],(err, resp) => {
    if (err) {
      console.log(err.stack);
      return res.json(err.stack);
