cc9a746cd8adc71f3f6e58630cfb2f834e198e43
yanccprogramador <yandepaula@gmail.com>
2017-08-10 22:15:48 -0300

    fixing redirect problems

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
 var redirectToHTTPS = require('express-http-to-https');
-var router = app;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
