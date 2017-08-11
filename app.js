7a602d05c4cf1d4be8692cb366f36d5cb3d03cb6
yanccprogramador <yandepaula@gmail.com>
2017-08-10 22:11:45 -0300

    fixing crypto problems

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
 var redirectToHTTPS = require('express-http-to-https');
-var router = app
+var router = app;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(redirectToHTTPS(null, null));
+app.use(redirectToHTTPS([''], ['']));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
