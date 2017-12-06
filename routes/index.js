cc7c8b5e34abf6d250a9d7e560345d8802b70355
yanccprogramador <yandepaula@gmail.com>
2017-12-06 19:35:35 -0200

    closing open and unused connections

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/search/:pc', function(req, res, next) {
         const search = { pc: req.params.pc };
         const query = client.query("select * from artigos where artigo ~*$1 OR Titulo ~*$1 order by id desc", [search.pc], (err, resp) => {
             if (err) {
+                done();
                 console.log(err.stack);
                 return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
             } else {
@@ -37,6 +38,7 @@ router.get('/meu/:Dono', function(req, res, next) {
         // SQL Query > Select Data
         const query = client.query("select * from artigos where Dono=$1 order by id desc", [id.dono], (err, resp) => {
           if (err) {
+            done();
                 console.log(err.stack);
                 return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
             } else {
@@ -56,6 +58,7 @@ router.get('/usuario', function(req, res, next) {
         }
         const query = client.query("Select nome from users", (err, resp) => {
            if (err) {
+            done();
                 console.log(err.stack);
                 return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
             } else {
@@ -75,6 +78,7 @@ router.get('/usuario/:login', function(req, res, next) {
         const par = { login: req.params.login };
         const query = client.query("select nome from users where login=$1", [par.login], (err, resp) => {
             if (err) {
+                done();
                 console.log(err.stack);
                 return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
             } else {
@@ -101,8 +105,10 @@ router.post('/usuario/logar/', function(req, res, next) {
                      }        
 
                      if (ssha256.check(resp.rows[0].senha,req.body.senha)){
+                        done();
                         return res.status(201).json({ success: true, data: "logado" });
                     } else {
+                        done();
                         return res.status(200).json({ success: false,data: "deslogado" });
                     }
 
@@ -110,9 +116,11 @@ router.post('/usuario/logar/', function(req, res, next) {
 
             });
         }else{
+            done();
              return res.status(500).json({data:"Mande todos os dados requeridos!"});
         }
          }else{
+            done();
             return res.status(500).json({success:false,data:"Por favor envie um json e na requisição insira no headers content-type application/json"});
         } 
         
@@ -130,6 +138,7 @@ router.post('/usuario/', function(req, res, next) {
             const data = { login: req.body.login, senha: ssha256.create(req.body.senha), nome: req.body.nome };
             const query = client.query("Insert into users values($1,$2,$3)", [data.login, data.senha, data.nome], (err, resp) => {
                 if (err) {
+                    done();
                     console.log(err.stack);
                     return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
                 } else {
@@ -138,9 +147,11 @@ router.post('/usuario/', function(req, res, next) {
                 }
             });
         }else{
+            done();
              return res.status(500).json({data:"Mande todos os dados requeridos!"});
         } 
         }else{
+            done();
             return res.status(500).json({success:true,data:"Por favor envie um json e na requisição insira no headers content-type application/json"});
         } 
     });
@@ -155,6 +166,7 @@ router.delete('/usuario/:login', function(req, res, next) {
         const id = { login: req.params.login };
         const query = client.query("delete from users where login=$1", [id.login], (err, resp) => {
             if (err) {
+                done();
                 console.log(err.stack);
                 return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
             } else {
@@ -177,6 +189,7 @@ router.put('/usuario/:login', function(req, res, next) {
             const data2 = { nome: req.body.nome, senha: ssha256.create(req.body.senha) };
             const query = client.query("UPDATE users SET nome = $1 , senha = $2 where login= $3 ", [data2.nome, data2.senha, id.login], (err, resp) => {
                 if (err) {
+                    done();
                     console.log(err.stack);
                     return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
                 } else {
@@ -185,9 +198,11 @@ router.put('/usuario/:login', function(req, res, next) {
                 }
             });
         }else{
+            done();
              return res.status(500).json({data:"Mande todos os dados requeridos!"});
         }
              }else{
+                done();
             return res.status(500).json({success:true,data:"Por favor envie um json e na requisição insira no headers content-type application/json"});
         } 
     });
@@ -205,6 +220,7 @@ router.get('/:id', function(req, res, next) {
         // SQL Query > Select Data
         const query = client.query("select * from artigos where Id=$1", [id.id], (err, resp) => {
             if (err) {
+                done();
                 console.log(err.stack);
                 return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
             } else {
@@ -227,6 +243,7 @@ router.get('/', (req, res, next) => {
         // SQL Query > Select Data
         const query = client.query("SELECT * FROM artigos order by id desc", (err, resp) => {
            if (err) {
+                done();
                 console.log(err.stack);
                 return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
             } else {
@@ -249,6 +266,7 @@ router.post('/', function(req, res, next) {
             const data = { title: req.body.title, dono: req.body.dono, artigo: req.body.artigo };
             const query = client.query("INSERT INTO artigos(Titulo,Dono,artigo) VALUES($1,$2,$3)", [data.title, data.dono, data.artigo], (err, resp) => {
                 if (err) {
+                    done();
                     console.log(err.stack);
                     return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
                 } else {
@@ -257,9 +275,11 @@ router.post('/', function(req, res, next) {
                 }
             });
         }else{
+            done();
              return res.status(500).json({data:"Mande todos os dados requeridos!"});
         }   
          }else{
+            done();
             return res.status(500).json({success:true,data:"Por favor envie um json e na requisição insira no headers content-type application/json"});
         }     
     });
@@ -274,6 +294,7 @@ router.delete('/:id', function(req, res, next) {
         const id = { id: req.params.id };
         const query = client.query("delete from artigos where Id=$1", [id.id], (err, resp) => {
             if (err) {
+                done();
                 console.log(err.stack);
                 return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
             } else {
@@ -300,6 +321,7 @@ router.put('/:id', function(req, res, next) {
             const data2 = { title: req.body.titulo, dono: req.body.dono, artigo: req.body.artigo };
             const query = client.query("UPDATE artigos SET titulo = $1 , dono = $2, artigo = $3  where id= $4 ", [data2.title, data2.dono, data2.artigo, id.id], (err, resp) => {
                 if (err) {
+                    done();
                     console.log(err.stack);
                     return res.status(400).json({ success: false, data: "Erro de query, verifique os dados e tente novamente!" });
                 } else {
@@ -308,9 +330,11 @@ router.put('/:id', function(req, res, next) {
                 }
             });
         }else{
+            done();
              return res.status(500).json({data:"Mande todos os dados requeridos!"});
         } 
         }else{
+            done();
             return res.status(500).json({success:true,data:"Por favor envie um json e na requisição insira no headers content-type application/json"});
         }    
     });
